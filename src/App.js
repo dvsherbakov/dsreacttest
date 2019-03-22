7dc4c334d7f033213788ccf22b7a26c395e056c1
Dmitry Sherbakov <dvsherbakov@gmail.com>
2019-03-22 21:47:53 +0500

    autit

--- src/App.js
+++ src/App.js
@@ -7,11 +7,20 @@ const API_Key = "82b797b6ebc625032318e16f1b42c016";
 //https://api.openweathermap.org/data/2.5/weather?q=Tyumen&appid=82b797b6ebc625032318e16f1b42c016&units=metric
 
 class App extends Component {
+
+  gettingWeather = async (e)=>{
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const api_url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_Key}&units=metric`);
+    const data = await api_url.json();
+    console.log(data);
+  }
+
   render() {
     return (
       <div className="App">
         <Info />
-        <Form />
+        <Form weatherMethod={this.gettingWeather}/>
       </div>
     );
   }
