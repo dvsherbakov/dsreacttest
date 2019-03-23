08abd8c6332082c08d295cea841eff3ce3b99052
Dmitry Sherbakov <dvsherbakov@gmail.com>
2019-03-23 17:58:34 +0500

    all test

--- src/App.js
+++ src/App.js
@@ -2,18 +2,38 @@ import React, { Component } from 'react';
 import './css/App.css';
 import Info from "./components/info";
 import Form from "./components/form"
+import Weather from "./components/weather"
 
 const API_Key = "82b797b6ebc625032318e16f1b42c016";
 //https://api.openweathermap.org/data/2.5/weather?q=Tyumen&appid=82b797b6ebc625032318e16f1b42c016&units=metric
 
 class App extends Component {
 
+  state = {
+    temp: undefined,
+    city: undefined,
+    country: undefined,
+    sunrise: undefined,
+    sunset: undefined,
+    error: undefined
+  }
+
   gettingWeather = async (e)=>{
     e.preventDefault();
     const city = e.target.elements.city.value;
-    const api_url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_Key}&units=metric`);
-    const data = await api_url.json();
-    console.log(data);
+    if (city){
+      const api_url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_Key}&units=metric`);
+      const data = await api_url.json();
+      console.log(data);
+      this.setState({
+        temp: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        sunrise: data.sys.sunrise,
+        sunset: data.sys.sunset,
+        error: ""
+      })
+    }
   }
 
   render() {
@@ -21,6 +41,14 @@ class App extends Component {
       <div className="App">
         <Info />
         <Form weatherMethod={this.gettingWeather}/>
+        <Weather 
+          temp={this.state.temp}
+          city={this.state.city}
+          country={this.state.country}
+          sunrise={this.state.sunrise}
+          sunset={this.state.sunset}
+          error={this.state.error}
+        />
       </div>
     );
   }
---
+++ src/components/weather.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+class Weather extends React.Component{
+    render(){
+        return(
+            <div>
+                { this.props.city &&
+                <div>
+                    <p>Город: {this.props.city}</p>
+                    <p>Страна: {this.props.country}</p>
+                    <p>Температура: {this.props.temp}</p>
+                    <p>Восход: {this.props.sunrise}</p>
+                    <p>Заход: {this.props.sunset}</p>
+                </div>
+                }
+            </div>
+            );
+        }
+    }
+    
+    export default Weather;
